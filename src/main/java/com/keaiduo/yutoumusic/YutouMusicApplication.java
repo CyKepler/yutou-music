7d7433e1c1ad1fd02e592b1edeee6f8602a06ba0
CyKepler <1748643254@qq.com>
2022-05-13 17:47:34 +0800

    add docker file

---
+++ src/main/java/com/keaiduo/yutoumusic/YutouMusicApplication.java
@@ -0,0 +1,14 @@
+package com.keaiduo.yutoumusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YutouMusicApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YutouMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keaiduo/yutoumusic/YutouMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keaiduo.yutoumusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YutouMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
